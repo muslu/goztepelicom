84a7f5045d4e5b0226d3803df7f44872cc67cfea
projeler <muslu.yuksektepe@makdos.com>
2025-01-20 14:15:52 +0300

    net-tools unzip wget curl

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 apt update
 apt upgrade -y
-apt install -y nginx python3-pip certbot python3-certbot-nginx supervisor
+apt install -y nginx python3-pip certbot python3-certbot-nginx supervisor net-tools unzip wget curl
 
 
 certbot certonly --nginx -d goztepeli.com -d www.goztepeli.com
