f09d466db1a7ac68548142358e0613d80e9e31fb
projeler <muslu.yuksektepe@makdos.com>
2025-01-20 14:04:57 +0300

    kurulum ayarları

---
+++ install/install.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade -y
+apt install -y nginx python3-pip certbot python3-certbot-nginx
+
+
+certbot certonly --nginx -d goztepeli.com -d www.goztepeli.com
+
+pip install -U tornado requests beautifulsoup4
