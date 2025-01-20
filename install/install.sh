39773de99c9bcb7e8f651275a9abbafb1bb0c730
projeler <muslu.yuksektepe@makdos.com>
2025-01-20 15:19:55 +0300

    cemir

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
 apt update
 apt upgrade -y
-apt install -y nginx python3-pip certbot python3-certbot-nginx supervisor net-tools unzip wget curl redis
+apt install -y nginx nginx-extras python3-pip certbot python3-certbot-nginx supervisor net-tools unzip wget curl redis
 
 systemctl enable redis-server && systemctl restart redis-server &&  systemctl status redis-server && netstat -pulten | grep redis
 
